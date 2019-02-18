8288805311eac9aad7e073ae6bf0e193fd742687
Lornatang <shiyipaisizuo@163.com>
2019-02-18 12:34:59 +0800

    Update net.py

--- net.py
+++ net.py
@@ -3,25 +3,25 @@ from torch import nn
 
 class Generate(nn.Module):
 
-    def __init__(self, noise=100):
+    def __init__(self, noise=300):
         super(Generate, self).__init__()
         self.layer1 = nn.Sequential(
-            nn.ConvTranspose2d(noise, 32 * 4, kernel_size=4, stride=1, padding=0),
-            nn.BatchNorm2d(32 * 4),
+            nn.ConvTranspose2d(noise, 64 * 4, kernel_size=4, stride=1, padding=0),
+            nn.BatchNorm2d(64 * 4),
             nn.ReLU(inplace=True)
         )
         self.layer2 = nn.Sequential(
-            nn.ConvTranspose2d(32 * 4, 32 * 2, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(32 * 2),
+            nn.ConvTranspose2d(64 * 4, 64 * 2, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(64 * 2),
             nn.ReLU(inplace=True)
         )
         self.layer3 = nn.Sequential(
-            nn.ConvTranspose2d(32 * 2, 32, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(32),
+            nn.ConvTranspose2d(64 * 2, 64, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(inplace=True)
         )
         self.classifier = nn.Sequential(
-            nn.ConvTranspose2d(32, 3, kernel_size=4, stride=2, padding=1),
+            nn.ConvTranspose2d(64, 3, kernel_size=4, stride=2, padding=1),
             nn.Tanh()
         )
 
@@ -38,21 +38,21 @@ class Discriminate(nn.Module):
     def __init__(self):
         super(Discriminate, self).__init__()
         self.layer1 = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size=4, stride=2, padding=1),
+            nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1),
             nn.LeakyReLU(negative_slope=1e-2, inplace=True)
         )
         self.layer2 = nn.Sequential(
-            nn.Conv2d(32, 32 * 2, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(32 * 2),
+            nn.Conv2d(64, 64 * 2, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(64 * 2),
             nn.LeakyReLU(negative_slope=1e-2, inplace=True)
         )
         self.layer3 = nn.Sequential(
-            nn.Conv2d(32 * 2, 32 * 4, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(32 * 4),
+            nn.Conv2d(64 * 2, 64 * 4, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(64 * 4),
             nn.LeakyReLU(negative_slope=1e-2, inplace=True)
         )
         self.classifier = nn.Sequential(
-            nn.Conv2d(32 * 4, 1, kernel_size=4, stride=2, padding=0),
+            nn.Conv2d(64 * 4, 1, kernel_size=4, stride=2, padding=0),
             nn.Sigmoid()
         )
 
